2504fbf1ae56b6066cec132a4b34638cca1df66a
YeomanYe <ming13821007683@gmail.com>
2017-08-04 22:58:47 +0800

    complete step-6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
-import { InMemorywebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component'
+import { HeroSearchComponent } from './hero-search.component';
 
 import {AppRoutingModule} from './app-routing.module';
 
@@ -22,10 +23,10 @@ import { AppComponent }  from './app.component';
   BrowserModule,
   FormsModule,
   HttpModule,
-  InMemorywebApiModule.forRoot(InMemoryDataService),
+  InMemoryWebApiModule.forRoot(InMemoryDataService),
   AppRoutingModule
   ],
-  declarations: [ AppComponent,HeroDetailComponent,HeroesComponent,DashboardComponent],
+  declarations: [ AppComponent,HeroDetailComponent,HeroesComponent,DashboardComponent,HeroSearchComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
